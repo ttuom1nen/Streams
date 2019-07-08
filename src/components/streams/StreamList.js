90139aca38525d7db1c85fa3f1035b5aea086ed2
ttuom1nen <ttuominen@outlook.com>
2019-07-08 22:41:51 +0300

    Stream list styling

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -7,12 +7,40 @@ class StreamList extends React.Component {
     this.props.fetchStreams();
   }
 
+  renderList() {
+    return this.props.streams.map(stream => {
+      return (
+        <li className="media p-3 border-bottom" key={stream.id}>
+          <img
+            src="https://via.placeholder.com/64"
+            className="mr-3"
+            alt="stream thumbnail"
+          />
+
+          <div className="media-body">
+            <h5 class="mt-0">{stream.title}</h5>
+            <div className="description text-muted">{stream.description}</div>
+          </div>
+        </li>
+      );
+    });
+  }
+
   render() {
-    return <div>StreamList</div>;
+    return (
+      <div>
+        <h2>Streams</h2>
+        <ul className="list-unstyled">{this.renderList()}</ul>
+      </div>
+    );
   }
 }
 
+const mapStateToProps = state => {
+  return { streams: Object.values(state.streams) };
+};
+
 export default connect(
-  null,
+  mapStateToProps,
   { fetchStreams }
 )(StreamList);
